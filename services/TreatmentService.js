d77f2d973cda609fb77758d94cdc540dcf43400f
martinTrejo <tincho_2690@hotmail.com>
2015-11-29 22:07:15 -0300

    Despues hay un punto del UML que tengo dudas si se podria mejorar

--- services/TreatmentService.js
+++ services/TreatmentService.js
@@ -9,6 +9,37 @@ require("../models/Patient");
 var mongoose = require("mongoose"),
     Treatment = mongoose.model("Treatment");
 
-var TreatmentService = {};
+var TreatmentService = {},
+    self = TreatmentService;
+
+UserService.addTreatment = function (req, res) {
+
+    "use strict";
+    var newTreatment = new Treatment();
+    var newPatient = new Patient();
+
+    newPatient.name = req.patientName;
+    newPatient.birthDate = req.patientBirthDate;
+    newPatient.description = req.patientDescription;
+
+/*
+El tratamiento tendria que tener asignado al usuario en el modelo. Ahora no se si el Perfil tambien tiene que tener asignado al usuario, no se, como que siento que falta una pata para especificar el perfil del usuario.
+*/
+
+    newTreatment.description = req.description;
+
+    newPatient.save(function(err, patient){
+        if (err) return console.error(err);
+        newTreatment.patient = patient._id;
+        return User.save(newUser).then(function (user) {
+            return user;
+        }, function (err) {
+            return err;
+        });
+    });
+}
+
+
+
 
 module.exports = TreatmentService;
