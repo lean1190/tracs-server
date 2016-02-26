18ee59bb508e7cb6c59771070ba0338d90e65e19
leian1306 <leian1306@gmail.com>
2016-02-26 17:26:51 -0300

    Cambié el endpoint para que sea un poco más expresivo

--- routers/PatientRouter.js
+++ routers/PatientRouter.js
@@ -1,10 +1,12 @@
+/*jshint bitwise: false, camelcase: true, curly: true, eqeqeq: true, globals: false, freeze: true, immed: true, nocomma: true, newcap: true, noempty: true, nonbsp: true, nonew: true, quotmark: double, undef: true, unused: true, strict: true, latedef: true*/
+
 /* globals require, module */
 
 var express = require("express"),
     router = express.Router(),
     PatientController = require("../controllers/PatientController");
 
-router.get("/:id", PatientController.findUserPatients);
+router.get("/user/:id", PatientController.findUserPatients);
 router.post("/", PatientController.add);
 
 module.exports = router;
