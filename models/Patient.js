05a1cfa79448a47a0910f4525dab776d696f4c1c
leian1306 <leian1306@gmail.com>
2016-03-08 18:55:08 -0300

    Se corrigió el tipo de dato del id de la persona cercana al paciente

--- models/Patient.js
+++ models/Patient.js
@@ -19,7 +19,7 @@ var PatientSchema = new Schema({
     treatment: [{ type: Schema.Types.ObjectId, ref : "Treatment" }],
     closestPeople: [
         {
-            personId: { type: Number, required: true },
+            personId: { type: String, required: true },
             name: { type: String, required: true },
             phone: { type: String, required: true },
             picture: { type: String, required: true },
