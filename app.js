8e5077976e81df28e52f1bf41ba51aedf098f723
tincho2690 <tincho_2690@hotmail.com>
2016-04-16 16:04:09 -0300

    Cambios para el chat global

--- app.js
+++ app.js
@@ -33,4 +33,27 @@ var app = new Application({path: __dirname, folder: "public"}, [
     {route: "/imAPatient", handler: ImAPatientRouter},
 ]);
 
+var io = require('socket.io')(4000);
+
+io.on('connection', function(socket){
+
+    socket.on('join:room', function(data){
+        var room_name = data.room_name;
+        socket.join(room_name);
+    });
+
+
+    socket.on('leave:room', function(msg){
+        msg.text = msg.user + " has left the room";
+        socket.in(msg.room).emit('exit', msg);
+        socket.leave(msg.room);
+    });
+
+
+    socket.on('send:message', function(msg){
+        socket.in(msg.room).emit('message', msg);
+    });
+
+});
+
 module.exports = app;
